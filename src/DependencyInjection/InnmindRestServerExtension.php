fc6ffb2ffa1db14c5ec7034d75e8d12bd9cc0ada
Baptiste Langlade <baptouuuu@gmail.com>
2016-06-26 16:37:12 +0200

    add config to register supported formats

--- src/DependencyInjection/InnmindRestServerExtension.php
+++ src/DependencyInjection/InnmindRestServerExtension.php
@@ -3,6 +3,14 @@ declare(strict_types = 1);
 
 namespace Innmind\Rest\ServerBundle\DependencyInjection;
 
+use Innmind\Rest\Server\Format\{
+    Format,
+    MediaType
+};
+use Innmind\Immutable\{
+    Map,
+    Set
+};
 use Symfony\Component\{
     HttpKernel\DependencyInjection\Extension,
     DependencyInjection\ContainerBuilder,
@@ -27,13 +35,13 @@ final class InnmindRestServerExtension extends Extension
             $configs
         );
 
-        $this->registerTypes($config['types'], $container);
+        $this
+            ->registerTypes($config['types'], $container)
+            ->registerAcceptFormats($config['accept'], $container)
+            ->registerContentTypeFormats($config['content_type'], $container);
     }
 
-    /**
-     * @return void
-     */
-    private function registerTypes(array $types, ContainerBuilder $container)
+    private function registerTypes(array $types, ContainerBuilder $container): self
     {
         $definition = $container->getDefinition(
             'innmind_rest_server.definition.types'
@@ -45,5 +53,58 @@ final class InnmindRestServerExtension extends Extension
                 [$type]
             );
         }
+
+        return $this;
+    }
+
+    private function registerAcceptFormats(
+        array $formats,
+        ContainerBuilder $container
+    ): self {
+        return $this->registerFormats(
+            'innmind_rest_server.formats.accept',
+            $formats,
+            $container
+        );
+    }
+
+    private function registerContentTypeFormats(
+        array $formats,
+        ContainerBuilder $container
+    ): self {
+        return $this->registerFormats(
+            'innmind_rest_server.formats.content_type',
+            $formats,
+            $container
+        );
+    }
+
+    private function registerFormats(
+        string $service,
+        array $formats,
+        ContainerBuilder $container
+    ): self {
+        $map = new Map('string', Format::class);
+
+        foreach ($formats as $format => $formatConfig) {
+            $mediaTypes = new Set(MediaType::class);
+
+            foreach ($formatConfig['media_types'] as $mediaType => $priority) {
+                $mediaTypes = $mediaTypes->add(
+                    new MediaType($mediaType, $priority)
+                );
+            }
+
+            $map = $map->put(
+                $format,
+                new Format($format, $mediaTypes, $formatConfig['priority'])
+            );
+        }
+
+        $container
+            ->getDefinition($service)
+            ->addArgument($map);
+
+        return $this;
     }
 }
--- tests/DependencyInjection/InnmindRestServerExtensionTest.php
+++ tests/DependencyInjection/InnmindRestServerExtensionTest.php
@@ -4,6 +4,8 @@ declare(strict_types = 1);
 namespace Tests\Innmind\Rest\ServerBundle\DependencyInjection;
 
 use Innmind\Rest\ServerBundle\DependencyInjection\InnmindRestServerExtension;
+use Innmind\Rest\Server\Format\Format;
+use Innmind\Immutable\MapInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class InnmindRestServerExtensionTest extends \PHPUnit_Framework_TestCase
@@ -18,6 +20,22 @@ class InnmindRestServerExtensionTest extends \PHPUnit_Framework_TestCase
             $extension->load(
                 [[
                     'types' => ['foo'],
+                    'accept' => [
+                        'json' => [
+                            'priority' => 0,
+                            'media_types' => [
+                                'application/json' => 0,
+                            ],
+                        ],
+                    ],
+                    'content_type' => [
+                        'json' => [
+                            'priority' => 0,
+                            'media_types' => [
+                                'application/json' => 0,
+                            ],
+                        ],
+                    ],
                 ]],
                 $container
             )
@@ -27,6 +45,50 @@ class InnmindRestServerExtensionTest extends \PHPUnit_Framework_TestCase
         $this->assertSame(
             ['register', ['foo']],
             $types->getMethodCalls()[0]
+        );
+        $accept = $container->getDefinition(
+            'innmind_rest_server.formats.accept'
+        );
+        $accept = $accept->getArgument(0);
+        $this->assertInstanceOf(MapInterface::class, $accept);
+        $this->assertSame('string', (string) $accept->keyType());
+        $this->assertSame(Format::class, (string) $accept->valueType());
+        $this->assertSame(1, $accept->size());
+        $this->assertSame('json', $accept->key());
+        $this->assertSame(
+            'json',
+            $accept->current()->name()
+        );
+        $this->assertSame(0, $accept->current()->priority());
+        $this->assertSame(
+            'application/json',
+            (string) $accept->current()->mediaTypes()->current()
+        );
+        $this->assertSame(
+            0,
+            $accept->current()->mediaTypes()->current()->priority()
+        );
+        $contentType = $container->getDefinition(
+            'innmind_rest_server.formats.content_type'
+        );
+        $contentType = $contentType->getArgument(0);
+        $this->assertInstanceOf(MapInterface::class, $contentType);
+        $this->assertSame('string', (string) $contentType->keyType());
+        $this->assertSame(Format::class, (string) $contentType->valueType());
+        $this->assertSame(1, $contentType->size());
+        $this->assertSame('json', $contentType->key());
+        $this->assertSame(
+            'json',
+            $contentType->current()->name()
+        );
+        $this->assertSame(0, $contentType->current()->priority());
+        $this->assertSame(
+            'application/json',
+            (string) $contentType->current()->mediaTypes()->current()
+        );
+        $this->assertSame(
+            0,
+            $contentType->current()->mediaTypes()->current()->priority()
         );
     }
 }
