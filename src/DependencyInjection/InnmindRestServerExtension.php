b60bc21e412675aa3c8701b68c3befa0503bb814
Baptiste Langlade <baptouuuu@gmail.com>
2016-06-26 14:54:28 +0200

    add basic bundle files

---
+++ src/DependencyInjection/InnmindRestServerExtension.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Rest\ServerBundle\DependencyInjection;
+
+use Symfony\Component\{
+    HttpKernel\DependencyInjection\Extension,
+    DependencyInjection\ContainerBuilder,
+    DependencyInjection\Loader,
+    Config\FileLocator
+};
+
+final class InnmindRestServerExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $config, ContainerBuilder $container)
+    {
+        $loader = new Loader\YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__ . '/../Resources/config')
+        );
+        $loader->load('services.yml');
+    }
+}
---
+++ src/InnmindRestServerBundle.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Rest\ServerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class InnmindRestServerBundle extends Bundle
+{
+}
