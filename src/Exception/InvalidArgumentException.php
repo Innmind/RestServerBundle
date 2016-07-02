4487c873316f50a4f8b3dd3193267fe63b7852bc
Baptiste Langlade <baptouuuu@gmail.com>
2016-07-02 17:31:28 +0200

    typo

--- src/Exception/InvalidArgumentException.php
+++ src/Exception/InvalidArgumentException.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types = 1);
 
-namespace Innmind\rest\ServerBundle\Exception;
+namespace Innmind\Rest\ServerBundle\Exception;
 
 class InvalidArgumentException extends \InvalidArgumentException implements ExceptionInterface
 {
