6bdd9ffc63f8e70f3cef2642f367cf3a6d9f7aa3
Baptiste Langlade <baptouuuu@gmail.com>
2016-07-03 14:44:05 +0200

    add listener to translate the response out of the controller

---
+++ src/EventListener/TranslateResponseListener.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Rest\ServerBundle\EventListener;
+
+use Innmind\Rest\ServerBundle\Translator\ResponseTranslator;
+use Innmind\Http\Message\ResponseInterface;
+use Symfony\Component\{
+    EventDispatcher\EventSubscriberInterface,
+    HttpKernel\KernelEvents,
+    HttpKernel\Event\GetResponseForControllerResultEvent
+};
+
+final class TranslateResponseListener implements EventSubscriberInterface
+{
+    private $translator;
+
+    public function __construct(ResponseTranslator $translator)
+    {
+        $this->translator = $translator;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::VIEW => [['translate', -255]],
+        ];
+    }
+
+    public function translate(GetResponseForControllerResultEvent $event)
+    {
+        $data = $event->getControllerResult();
+
+        if (!$data instanceof ResponseInterface) {
+            return;
+        }
+
+        $event->setResponse(
+            $this->translator->translate($data)
+        );
+    }
+}
---
+++ tests/EventListener/TranslateResponseListenerTest.php
@@ -0,0 +1,93 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\Rest\ServerBundle\EventListener;
+
+use Innmind\Rest\ServerBundle\{
+    EventListener\TranslateResponseListener,
+    Translator\ResponseTranslator
+};
+use Innmind\Http\{
+    Message\Response,
+    Message\StatusCode,
+    Message\ReasonPhrase,
+    ProtocolVersion,
+    Headers,
+    Header\HeaderInterface,
+    Header\ContentType,
+    Header\ContentTypeValue,
+    Header\ParameterInterface
+};
+use Innmind\Filesystem\Stream\StringStream;
+use Innmind\Immutable\Map;
+use Symfony\Component\{
+    EventDispatcher\EventSubscriberInterface,
+    HttpKernel\KernelEvents,
+    HttpKernel\Event\GetResponseForControllerResultEvent,
+    HttpKernel\HttpKernelInterface,
+    HttpFoundation\Request
+};
+
+class TranslateResponseListener extends \PHPUnit_Framework_TestCase
+{
+    public function testInterface()
+    {
+        $listener = new TranslateResponseListener(new ResponseTranslator);
+
+        $this->assertInstanceOf(EventSubscriberInterface::class, $listener);
+    }
+
+    public function testGetSubscribedEvents()
+    {
+        $this->assertSame(
+            [KernelEvents::VIEW => [['translate', -255]]],
+            TranslateResponseListener::getSubscribedEvents()
+        );
+    }
+
+    public function testTranslate()
+    {
+        $listener = new TranslateResponseListener(new ResponseTranslator);
+        $event = new GetResponseForControllerResultEvent(
+            $this->getMock(HttpKernelInterface::class),
+            new Request,
+            HttpKernelInterface::MASTER_REQUEST,
+            new Response(
+                new StatusCode(200),
+                new ReasonPhrase('OK'),
+                new ProtocolVersion(1, 1),
+                new Headers(
+                    (new Map('string', HeaderInterface::class))
+                        ->put(
+                            'content-type',
+                            new ContentType(
+                                new ContentTypeValue(
+                                    'application',
+                                    'json',
+                                    new Map('string', ParameterInterface::class)
+                                )
+                            )
+                        )
+                ),
+                new StringStream('{"answer":42}')
+            )
+        );
+
+        $this->assertSame(null, $listener->translate($event));
+        $this->assertTrue($event->hasResponse());
+    }
+
+    public function testDoesntTranslate()
+    {
+        $listener = new TranslateResponseListener(new ResponseTranslator);
+        $event = new GetResponseForControllerResultEvent(
+            $this->getMock(HttpKernelInterface::class),
+            new Request,
+            HttpKernelInterface::MASTER_REQUEST,
+            null
+        );
+
+        $this->assertSame(null, $listener->translate($event));
+        $this->assertFalse($event->hasResponse());
+    }
+}
